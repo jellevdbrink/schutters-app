b98359b5fba76b9787c11170140270fb96556536
Jelle <jelle@famvandenbrink.net>
2025-05-20 16:28:13 +0200

    general structure and routes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,19 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterOutlet } from '@angular/router';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faHome, faBasketball, faNewspaper, faUser } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, RouterLink, FontAwesomeModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
+  protected faHome = faHome;
+  protected faBasketball = faBasketball;
+  protected faNewspaper = faNewspaper;
+  protected faUser = faUser;
+
   title = 'schutters-app';
 }
